48bf52973ddaf86b7b707a77d4766bea52b1a88d
ravirajgohil <raviraj232@gmail.com>
2019-07-13 22:57:23 +0530

    MinMaxAI_comment
    Added comments for methods
    removed used playerSign attribute from method isGameOver

--- MinMaxAI.py
+++ MinMaxAI.py
@@ -1,35 +1,42 @@
-class MinMaxAI():
+class MinMaxAI:
+    # This method returns move based on MinMax algorithm
+    # Input
+    #   1. boardStatus - Current board status
+    #   2. playerSign - player sign
+    #   3. maximizerPlayer - maximizer of minimizer player flag
+    #   4. level - current level of tree
+    # Output
+    #   1. best possible move
     @staticmethod
-    def __getMoveAlphaBeta__(boardStatus,playerSign,maximizerPlayer,level = 1):
-        maxVal = { 'index' : -1, 'value' : -10}
-        minVal = { 'index' : -1, 'value' : 10}
+    def __getMoveAlphaBeta__(boardStatus, playerSign, maximizerPlayer, level=1):
+        maxVal = {'index': -1, 'value': -10}
+        minVal = {'index': -1, 'value': 10}
 
-        for i in range(0,9):
+        for i in range(0, 9):
             if boardStatus[i] != " ":
                 continue
             
             boardStatus[i] = playerSign
-            #print(boardStatus)
 
-            winner = MinMaxAI.__isGameOver__(boardStatus,playerSign)
+            winner = MinMaxAI.__isGameOver__(boardStatus)
             
             if winner == -1:
-                return { 'index' : i , 'value' : 0}
-            elif winner in('O','X'): 
+                return {'index': i, 'value': 0}
+            elif winner in('O', 'X'):
                 if maximizerPlayer:
                     if winner == playerSign:
-                        result = { 'index' : i , 'value' : 10/level}
+                        result = {'index': i, 'value': 10/level}
                     else:
-                        result = { 'index' : i , 'value' : -10/level}
+                        result = {'index': i, 'value': -10/level}
 
                     if result['value'] >= maxVal['value']:
                         maxVal['value'] = result['value']
                         maxVal['index'] = i
                 else:
                     if winner == playerSign:
-                        result = { 'index' : i , 'value' : -10/level}
+                        result = {'index': i, 'value': -10/level}
                     else:
-                        result = { 'index' : i , 'value' : 10/level}
+                        result = {'index': i, 'value': 10/level}
 
                     if result['value'] <= minVal['value']:
                         minVal['value'] = result['value']
@@ -41,7 +48,7 @@ class MinMaxAI():
                 newplayerSign = 'X' if playerSign == 'O' else 'O'
                 newlevel = level + 1
                 newBoardStatus = boardStatus.copy()
-                result = MinMaxAI.__getMoveAlphaBeta__(newBoardStatus,newplayerSign,not maximizerPlayer,newlevel)
+                result = MinMaxAI.__getMoveAlphaBeta__(newBoardStatus, newplayerSign, not maximizerPlayer, newlevel)
                 if maximizerPlayer:
                     if result['value'] >= maxVal['value']:
                         maxVal['value'] = result['value']
@@ -52,40 +59,35 @@ class MinMaxAI():
                         minVal['index'] = i
                         
                 boardStatus[i] = " "
-                #print(result)
         return maxVal if maximizerPlayer else minVal
- 
-        
+
+
+    # This method return if the game is active or not flag
+    # Input
+    #   1. current board status
+    # Output
+    #   1. game activation status
     @staticmethod
-    def __isGameOver__(board,playerSign):
-        if ( board[0] == board[1] == board[2] ) and board[0] != " ":
-            ##addWinner(checkValAt(0))
+    def __isGameOver__(board):
+        if (board[0] == board[1] == board[2]) and board[0] != " ":
             return board[0]
-        elif ( board[3] == board[4] == board[5] ) and board[3] != " ":
-            ##addWinner(checkValAt(3))
-            return  board[3]
-        elif ( board[6] == board[7] == board[8] ) and board[6] != " ":
-            ##addWinner(checkValAt(6))
-            return  board[6]
-        elif ( board[0] == board[3] == board[6] ) and board[0] != " ":
-            ##addWinner(checkValAt(0))
+        elif (board[3] == board[4] == board[5]) and board[3] != " ":
+            return board[3]
+        elif (board[6] == board[7] == board[8]) and board[6] != " ":
+            return board[6]
+        elif (board[0] == board[3] == board[6]) and board[0] != " ":
             return board[0]
-        elif ( board[1] == board[4] == board[7] ) and board[1] != " ":
-            ##addWinner(checkValAt(1))
+        elif (board[1] == board[4] == board[7]) and board[1] != " ":
             return board[1]
-        elif ( board[2] == board[5] == board[8] ) and board[2] != " ":
-            ##addWinner(checkValAt(2))
+        elif (board[2] == board[5] == board[8]) and board[2] != " ":
             return board[2]
-        elif ( board[0] == board[4] == board[8] ) and board[0] != " ":
-            ##addWinner(checkValAt(0))
+        elif (board[0] == board[4] == board[8]) and board[0] != " ":
             return board[0]
-        elif ( board[2] == board[4] == board[6] ) and board[2] != " ":
-            ##addWinner(checkValAt(2))
+        elif (board[2] == board[4] == board[6]) and board[2] != " ":
             return board[2]
         elif " " not in board:
-            ##addWinner(0)
             return -1
         else:
             return 0
 
-print(MinMaxAI.__getMoveAlphaBeta__(['X'," ",'X'," ",'O'," "," ",'O','X'],'O',True))
+#print(MinMaxAI.__getMoveAlphaBeta__(['X'," ",'X'," ",'O'," "," ",'O','X'],'O',True))
